2437bc0dab29ac562f528ca0a77b390be25c80e7
Zeesat <alauddinadhaps@gmail.com>
2026-02-04 01:11:42 +0700

    1.0.2: Add sending chat from Discord to Terraria; Fully working; Can't send msg more than one line

--- New file.py
+++ New file.py
@@ -5,6 +5,10 @@ import asyncio
 import threading 
 import subprocess
 import time
+import re
+
+# Wait for Terraria Server Start 
+time.sleep(3)
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
@@ -49,23 +53,28 @@ def terminal_input():
 # cooldown = False
 # lines = []   
 
+pattern = r"^((?:\d{1,3}\.){3}\d{1,3})(:\d+)"
 def baca_log():
     global cooldown, lines
-    with open("terraria.log", "r", encoding="utf-8", errors="ignore") as f:
+    with open("Terraria.log", "r", encoding="utf-8", errors="ignore") as f:
         f.seek(0, os.SEEK_END)
         while True:
         #    if cooldown is False:
                 line = f.readline()
                 if line:
-                    asyncio.run_coroutine_threadsafe(
-                        kirim_ke_discord(line.strip()),
-                        client.loop
-                    )
-                    cooldown = True
-                    lines = []
+                    line = re.sub(pattern, "xxx.xxx.xxx.xxx\\2", line)
+                    if line == ": \n" or line == ": " or line == ":":
+                        continue
+                    if ("<Server>" not in line) and ("Invalid command." not in line):
+                        asyncio.run_coroutine_threadsafe(
+                            kirim_ke_discord(line.strip().replace("\n", " ")),
+                            client.loop
+                        )
+                    # cooldown = True
+                    # lines = []
                 else:
                     time.sleep(0.1)
-                    
+
             # else:
             #     line = f.readline()
             #     if line:
@@ -75,18 +84,24 @@ def baca_log():
 
                 
 
-
-def baca_discord():
-
-
 @client.event
 async def on_ready():
-    print(f"Login sebagai {client.user}")
-
+    # print(f"Login as {client.user}", flush=False)
+    asyncio.run_coroutine_threadsafe(
+    kirim_ke_discord(f"Login as {client.user}"),
+    client.loop)
 
 
     threading.Thread(target=baca_log, daemon=True).start()
-    threading.Thread(target=terminal_input, daemon=True).start()
+   # threading.Thread(target=terminal_input, daemon=True).start()
    # threading.Thread(target=cooldown_timer, daemon=True).start()
-    
+
+@client.event
+async def on_message(message):
+    if message.author.bot:   # ignore bot msg
+        return
+    if message.channel.id == CHANNEL_ID:
+        print(f"say [c/7289DA:<{message.author.display_name}>] {message.content}", flush=True)
+
+
 client.run(DISCORD_TOKEN)
--- launcher.c
+++ launcher.c
@@ -4,6 +4,8 @@
 #include <fcntl.h>
 
 int main(int argc, char *argv[]) {
+int cmd_pipe[2];
+pipe(cmd_pipe);  // cmd_pipe[0] = baca, cmd_pipe[1] = tulis
 
     pid_t p = fork();
 
@@ -23,8 +25,12 @@ int main(int argc, char *argv[]) {
         dup2(fd, STDOUT_FILENO);
         dup2(fd, STDERR_FILENO);
         close(fd);
+        close(cmd_pipe[1]);              // child hanya baca
+        dup2(cmd_pipe[0], STDIN_FILENO); // arahkan stdin ke pipe
+        close(cmd_pipe[0]);
 
-        execv("./TerrariaServer.bin.x86_64.real", argv);
+        argv[0] = "./TerrariaServer.bin.x86_64.real";
+        execv(argv[0], argv);
 
         perror("exec Terraria failed");
         _exit(1);
@@ -33,10 +39,17 @@ int main(int argc, char *argv[]) {
     pid_t p2 = fork();
 
     if (p2 == 0) {
+        close(cmd_pipe[0]);              // logger hanya tulis
+        dup2(cmd_pipe[1], STDOUT_FILENO); // logger "print" = kirim ke Terraria
+        close(cmd_pipe[1]);
+
         execl("./logger", "logger", NULL);
         perror("exec logger failed");
         _exit(1);
     }
+    
+    close(cmd_pipe[0]);
+    close(cmd_pipe[1]);
 
     waitpid(p, NULL, 0);
     waitpid(p2, NULL, 0);
